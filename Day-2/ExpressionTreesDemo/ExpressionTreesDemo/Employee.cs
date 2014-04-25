efc6f0590ba3480e8febfbdcf721a6e10359e5b1
tkmagesh <tkmagesh77@gmail.com>
2014-04-25 14:45:33 +0530

    Expression Trees Demo

---
+++ Day-2/ExpressionTreesDemo/ExpressionTreesDemo/Employee.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ExpressionTreesDemo
+{
+    public class Employee : INotifyPropertyChanged
+    {
+        private string _firstName;
+        private string _lastName;
+        public string FirstName
+        {
+            set {
+                _firstName = value;
+                PropertyChanged(this, new PropertyChangedEventArgs(Name.Of<Employee>(e => e.FirstName)));
+                PropertyChanged(this, new PropertyChangedEventArgs(Name.Of<Employee>(e => e.FullName)));
+            }
+            get
+            {
+                return _firstName;
+            }
+        }
+
+        public string LastName {
+            set
+            {
+                _lastName = value;
+                PropertyChanged(this, new PropertyChangedEventArgs(Name.Of<Employee>(e => e.LastName)));
+                PropertyChanged(this, new PropertyChangedEventArgs(Name.Of<Employee>(e => e.FullName)));
+            }
+            get
+            {
+                return _lastName;
+            }
+        }
+
+        public string FullName {
+            get
+            {
+                return this.FirstName + " " + this.LastName;
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged = delegate {};
+    }
+
+    public static class Name {
+        public static string Of<T>(Expression<Func<T,object>> exp) {
+            var memberExp = exp.Body as MemberExpression;
+            return memberExp.Member.Name;
+        }
+    }
+}
